395569af56cea53ec4d31e06def636ddbad55bf7
riteshpaarihar <ritesh.rs199@gmail.com>
2025-02-22 22:47:15 +0530

    add card schema

--- src/schema/cardSchema.js
+++ src/schema/cardSchema.js
@@ -4,7 +4,16 @@ const CardSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-    }
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+
+    },
 
 }, {
     timestamps: true,
