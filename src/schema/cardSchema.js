622a847aabda55d945ed277198eb53f11a1ca110
riteshpaarihar <ritesh.rs199@gmail.com>
2025-02-26 22:13:56 +0530

    add card schema

--- src/schema/cardSchema.js
+++ src/schema/cardSchema.js
@@ -1,20 +1,28 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import Product from "./productSchema";
 
 const CardSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
-    description: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-
-    },
+    items: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1,
+        }
+    }]
 
 }, {
     timestamps: true,
 })
+
+const Card = mongoose.model("Card", CardSchema);
+export default Card;
